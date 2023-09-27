5b75c4edafc2c55774c8a3e53e246cb37a32906d
Ann-Silje Kirkvik <kirann@kartverket.no>
2023-09-27 14:38:12 +0200

    Update scale script for new VLBI naming convention

--- analysis/vlbi_scale/vlbi_scale.py
+++ analysis/vlbi_scale/vlbi_scale.py
@@ -43,7 +43,7 @@ config.files.profiles = [pipeline]
 config.read_pipeline(pipeline)
 
 # Get data from timeseries dataset
-dset_ts = dataset.Dataset.read(rundate=date(1970,1,1), pipeline=pipeline, stage="timeseries", label="0", session="", id=dset_id)
+dset_ts = dataset.Dataset.read(rundate=date(1970,1,1), pipeline=pipeline, stage="timeseries", label="0", session_code="", id=dset_id)
 
 # Select and discard sessions
 idx_volume = dset_ts.network_volume > volume_limit
@@ -53,6 +53,8 @@ idx_1 = np.logical_and(np.logical_and(idx_volume, idx_all_good), ~idx_nan)
 scale_rms = np.sqrt(np.mean(dset_ts.neq_helmert_scale[idx_1]**2))
 idx_rms = np.abs(dset_ts.neq_helmert_scale) < rms_limit*scale_rms
 idx_2 = np.logical_and(idx_1, idx_rms)
+sort_idx_2 = np.argsort(dset_ts.rundate[idx_2])
+
 
 print(f"Total number of sessions: {np.sum(idx_all_good)}")
 print(f"Discaring sessions with network volume smaller than 10Mm^3")
@@ -80,7 +82,7 @@ print(f"After {discont_epoch.utc.decimalyear}:  Slope: {result_after.slope: 6.4f
 
 # Plot scale with linear regression
 plt.figure(figsize=(12,8), dpi=150)
-plt.scatter(dset_ts.time.utc.datetime[idx_2], dset_ts.neq_helmert_scale[idx_2], c=dset_ts.network_volume[idx_2], marker=".")
+plt.scatter(dset_ts.time.utc.datetime[idx_2][sort_idx_2], dset_ts.neq_helmert_scale[idx_2][sort_idx_2], c=dset_ts.network_volume[idx_2][sort_idx_2], marker=".")
 plt.axvline(discont_epoch.utc.datetime, color="black")
 plt.plot(dset_ts.time.utc.datetime[idx_3], fitted_before, color="red")
 plt.plot(dset_ts.time.utc.datetime[idx_4], fitted_after, color="red")
@@ -105,7 +107,7 @@ units = ["mm", "mm", "mm", "mas", "mas", "mas"]
 for f, u in zip(fields,units):
     name = f.replace("neq_helmert_", "")
     plt.figure(figsize=(12,8), dpi=150)
-    plt.scatter(dset_ts.time[idx_2].utc.datetime, dset_ts[f][idx_2], c=dset_ts.network_volume[idx_2], marker=".")
+    plt.scatter(dset_ts.time[idx_2][sort_idx_2].utc.datetime, dset_ts[f][idx_2][sort_idx_2], c=dset_ts.network_volume[idx_2][sort_idx_2], marker=".")
     cbar = plt.colorbar()
     cbar.set_label(f"Network volume [Mm^3]")
     plt.title(f"{name}")
@@ -124,9 +126,8 @@ start_epoch = dset_ts.time[idx_2].min.utc.datetime
 end_epoch = dset_ts.time[idx_2].max.utc.datetime
 running_median = np.full(len(dset_ts.time[idx_2]), fill_value=np.nan)
 
-# TODO: sort dset_ts.time[idx_2] and dset_ts.neq_helmert_scale[idx]
 
-for i, t in enumerate(dset_ts.time[idx_2].utc.datetime):
+for i, t in enumerate(dset_ts.time[idx_2][sort_idx_2].utc.datetime):
     time_lower_limit = t - delta
     if time_lower_limit < start_epoch:
         continue
@@ -135,15 +136,15 @@ for i, t in enumerate(dset_ts.time[idx_2].utc.datetime):
     if time_upper_limit > end_epoch:
         continue
 
-    idx_time_lower = dset_ts.time[idx_2].utc.datetime > time_lower_limit
-    idx_time_upper = dset_ts.time[idx_2].utc.datetime < time_upper_limit
+    idx_time_lower = dset_ts.time[idx_2][sort_idx_2].utc.datetime > time_lower_limit
+    idx_time_upper = dset_ts.time[idx_2][sort_idx_2].utc.datetime < time_upper_limit
     idx_interval = np.logical_and(idx_time_lower, idx_time_upper)
     #print(f"{np.sum(idx_interval)} sessions in interval {time_lower_limit:%Y-%m-%d}-{time_upper_limit:%Y-%m-%d}")
-    running_median[i] = np.median(dset_ts.neq_helmert_scale[idx_2][idx_interval])
+    running_median[i] = np.median(dset_ts.neq_helmert_scale[idx_2][sort_idx_2][idx_interval])
 
 plt.figure(figsize=(12,8), dpi=150)
-plt.plot(dset_ts.time[idx_2].utc.datetime, running_median, color="red")
-plt.scatter(dset_ts.time[idx_2].utc.datetime, dset_ts.neq_helmert_scale[idx_2], c=dset_ts.network_volume[idx_2], marker=".")
+plt.plot(dset_ts.time[idx_2][sort_idx_2].utc.datetime, running_median, color="red")
+plt.scatter(dset_ts.time[idx_2][sort_idx_2].utc.datetime, dset_ts.neq_helmert_scale[idx_2][sort_idx_2], c=dset_ts.network_volume[idx_2][sort_idx_2], marker=".")
 cbar = plt.colorbar()
 cbar.set_label(f"Network volume [Mm^3]")
 plt.title(f"VLBI Scale with running median ({days} days)")
@@ -158,19 +159,17 @@ plt.close()
 if make_csv:
     filename = f"{csvdir}/VLBI_scale_{rms_limit}_{volume_limit}_{dset_id}.csv"
     with open(filename, "w") as fid:
-        header = "date, dbc, scale [ppb], t_x [m], t_y [m], t_z [m], r_1 [mas], r_2 [mas], r_3 [mas], volume [Mm^3]"
+        header = f"date, session code, scale [ppb], t_x [m], t_y [m], t_z [m], r_1 [mas], r_2 [mas], r_3 [mas], volume [Mm^3]"
         fid.write(f"{header}\n")
-        rundate = dset_ts.rundate[idx_2]
-        sort_idx = np.argsort(rundate)
-        rundate = rundate[sort_idx]
-        session = dset_ts.session[idx_2][sort_idx]
-        scale = dset_ts.neq_helmert_scale[idx_2][sort_idx]
-        t_x = dset_ts.neq_helmert_T_X[idx_2][sort_idx]
-        t_y = dset_ts.neq_helmert_T_Y[idx_2][sort_idx]
-        t_z = dset_ts.neq_helmert_T_Z[idx_2][sort_idx]
-        r_1 = dset_ts.neq_helmert_alpha[idx_2][sort_idx]
-        r_2 = dset_ts.neq_helmert_beta[idx_2][sort_idx]
-        r_3 = dset_ts.neq_helmert_gamma[idx_2][sort_idx]
-        volume = dset_ts.network_volume[idx_2][sort_idx]
-        for rd, dbc, s, tx, ty, tz, r1, r2, r3, v in zip(rundate, session, scale, t_x, t_y, t_z, r_1, r_2, r_3, volume):
-            fid.write(f"{rd}, {dbc}, {s}, {tx}, {ty}, {tz}, {r1}, {r2}, {r3}, {v} \n")
+        rundate = dset_ts.rundate[idx_2][sort_idx_2]
+        session_code = dset_ts.session_code[idx_2][sort_idx_2]
+        scale = dset_ts.neq_helmert_scale[idx_2][sort_idx_2]
+        t_x = dset_ts.neq_helmert_T_X[idx_2][sort_idx_2]
+        t_y = dset_ts.neq_helmert_T_Y[idx_2][sort_idx_2]
+        t_z = dset_ts.neq_helmert_T_Z[idx_2][sort_idx_2]
+        r_1 = dset_ts.neq_helmert_alpha[idx_2][sort_idx_2]
+        r_2 = dset_ts.neq_helmert_beta[idx_2][sort_idx_2]
+        r_3 = dset_ts.neq_helmert_gamma[idx_2][sort_idx_2]
+        volume = dset_ts.network_volume[idx_2][sort_idx_2]
+        for rd, sc, s, tx, ty, tz, r1, r2, r3, v in zip(rundate, session_code, scale, t_x, t_y, t_z, r_1, r_2, r_3, volume):
+            fid.write(f"{rd}, {sc}, {s}, {tx}, {ty}, {tz}, {r1}, {r2}, {r3}, {v} \n")
