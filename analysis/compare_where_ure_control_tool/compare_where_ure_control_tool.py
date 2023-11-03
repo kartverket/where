5a0d46785debdf5a20872eba465f45e113176139
Michael Dähnn <michael.daehnn@kartverket.no>
2023-11-03 15:29:38 +0100

    update

--- analysis/compare_where_ure_control_tool/compare_where_ure_control_tool.py
+++ analysis/compare_where_ure_control_tool/compare_where_ure_control_tool.py
@@ -7,20 +7,20 @@ Description:
 """
 # Standard library imports
 from datetime import date
-import subprocess
+from pathlib import Path
 
 # External library imports
 import numpy as np
 
-# Where imports
-from where import data
-from where import parsers
-from where.lib import log
-
-TECH = "sisre"
+# Midgard imports
+from midgard import parsers
+from midgard.data import dataset
+from midgard.dev import log
 
 
 def _difference(dset_where, dset_ure_tool):
+    
+    file_path = "./work/diff_where_ure_control_tool"
 
     if dset_where.num_obs == 0 or dset_ure_tool == 0:
         log.warn(
@@ -29,50 +29,28 @@ def _difference(dset_where, dset_ure_tool):
         )
         return 1
 
-    ddiff = dset_where.difference_with(dset_ure_tool, difference_by=["time", "satellite"])
-    ddiff.write_as(tech=TECH, stage="dwhr_ure")
+    ddiff = dset_where.difference(dset_ure_tool, index_by="time, satellite")
+    log.info(f"Write dataset with difference of Where and URE control tool fields: {file_path}")
+    ddiff.write(file_path)
 
 
-def _generate_where_dset(rundate, nav_type, system, frequencies):
+def _generate_where_dset(inpath, rundate):
     """Generate Where dataset
 
-    Start Where analysis with Where and read Dataset.
     """
-    program = "where"
-    session_name = "brdm"
-
-    # Start processing with Where
-    where_args = [
-        str(rundate.year),
-        str(rundate.month),
-        str(rundate.day),
-        "--" + TECH,
-        "--session_name=" + session_name,
-        "--brdc_block_nearest_to=transmission_time:positive",
-        "--systems=" + system,
-        "--frequencies=" + frequencies,
-        "--navigation_message_type=" + system + ":" + nav_type,
-        "--clock_product=sp3",
-        "--weight_factor_elev_mask=5",
-        "--profile=grc",
-        "-N",
-        "-D",
-        "-T",
-    ]
-    log.info("Start {}: {} {}".format(TECH.upper(), program, " ".join(where_args)))
-    process = subprocess.run([program] + where_args)
-    if process.returncode:
-        log.error(f"Where {TECH.upper()} failed with error code {process.returncode} ({' '.join(process.args)})")
-
-    # Write SISRE Dataset
-    dset_vars = dict(rundate=rundate, tech="sisre", stage="calculate", session="", dataset_name="", dataset_id="last")
-    dset = data.Dataset(**dset_vars)
+    log.info(f"Read Where dataset file: {inpath}")
+    
+    if not inpath.exists():
+        log.fatal(f"File path {inpath} does not exists.")
+    
+    # Define dataset
+    dset = dataset.Dataset().read(inpath)
     dset.add_float(
-        "sqrt_a2_c2", val=(np.sqrt(dset["orb_diff_acr"].itrs[:, 0] ** 2 + dset["orb_diff_acr"].itrs[:, 1] ** 2))
+        "sqrt_a2_c2", val=(np.sqrt(dset.orb_diff.acr.along ** 2 + dset.orb_diff.acr.cross ** 2))
     )
-    dset.add_float("dradial", val=dset["orb_diff_acr"].itrs[:, 2])
+    dset.add_float("dradial", val=dset.orb_diff.acr.radial)
     dset.add_float("clk_diff_dt_mean", val=dset["clk_diff"] - dset["clk_diff_with_dt_mean"])
-    dset.write_as(tech=dset.vars["tech"], stage="where")
+    dset.write("./result/gnss-{:%Y%m%d}-where-raw-dataset.hdf5".format(rundate))
 
     return dset
 
@@ -82,15 +60,14 @@ def _generate_ure_control_tool_dset(inpath, rundate):
 
     Read URE control tool output file and generate afterwards Dataset.
     """
-    log.info("Read URE control tool output file and generate afterwards Dataset.")
+    log.info(f"Read URE control tool output file: {inpath}")
+    
+    if not inpath.exists():
+        log.fatal(f"File path {inpath} does not exists.")
+        
     p = parsers.parse_file(parser_name="ure_control_tool_csv", file_path=inpath)
-
-    # Define dataset
-    dset = data.Dataset(rundate=rundate, tech="sisre", stage="ure_tool", dataset_name="", dataset_id=0, empty=True)
-    p.write_to_dataset(dset)
-
-    # Write Dataset to file
-    dset.write()
+    dset = p.as_dataset()
+    dset.write("./result/gnss-{:%Y%m%d}-uretool-raw-dataset.hdf5".format(rundate))
 
     return dset
 
@@ -107,19 +84,18 @@ if __name__ == "__main__":
     """
     dset_diff_merged = None
     log.init(log_level="info")
-    year = 2019
-    month = 3
+    year = 2023
+    month = 5
     day = 1
     rundate = date(year, month, day)
-    nav_type = "FNAV"  # INAV, FNAV, LNAV
-    system = "E"  # E, G
-    frequencies = "E:E1_E5a"  # E:E1 (INAV), E:E1_E5b (INAV), E:E1_E5a (FNAV), G:L1_L2 (LNAV)
-    if nav_type == "FNAV":
-        inpath = "./data/G_GAL258_E1E5a_URE-AllPRN_{:%y%m%d}.csv".format(rundate)
+    
+    where_path = Path("./data/sisre-{:%Y%m%d}-calculate-raw-dataset.hdf5".format(rundate))
+    #ure_path = Path("./data/G_GAL258_E1E5a_URE-AllPRN_{:%Y%m%d}.csv".format(rundate))
+    ure_path = Path("./data/G_GPS_LNAVP1-P2_model_{:%Y%m%d}_120_None_None_OS.csv".format(rundate))
 
     # Generate datasets
-    dset_ure_tool = _generate_ure_control_tool_dset(inpath, rundate)
-    dset_where = _generate_where_dset(rundate, nav_type, system, frequencies)
+    dset_ure_tool = _generate_ure_control_tool_dset(ure_path, rundate)
+    dset_where = _generate_where_dset(where_path, rundate)
 
     # Generate difference between datasets
     _difference(dset_where, dset_ure_tool)
