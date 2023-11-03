eb68cc70ffc8b0775e98786fec8108ebff0f16b5
Michael Dähnn <michael.daehnn@kartverket.no>
2023-11-03 15:28:14 +0100

    removed unnecessary code

--- where/apriori/orbit/has.py
+++ where/apriori/orbit/has.py
@@ -18,7 +18,6 @@ Example:
 # Standard library imports
 from datetime import timedelta
 from typing import List, Union
-from pathlib import Path, PosixPath
 
 # External library imports
 import numpy as np
@@ -710,9 +709,6 @@ class HasOrbit(orbit.AprioriOrbit):
             )
 
             cleaners.apply_remover("ignore_satellite", dset, satellites=not_available_sat)
-
-        # Prepare mjd data
-        mjd = self._add_dim(self.dset_edit[time_key].gps.mjd)
 
         # Determine HAS message index for a given satellite, observation epoch and eventually system/signal
         indices = np.full(dset.num_obs, -1, dtype=int) # -1 is chosen to guarantee that not a wrong index is used 
